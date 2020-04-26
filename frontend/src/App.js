c86f050168cfe1437f9372b10fadf1eab485577c
dthemg <david.montgomery@live.se>
2020-04-26 18:40:30 +0200

    Lets use functional style like before

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,38 +4,22 @@ import SignIn from './components/signInComponent';
 import SignUp from './components/signUpComponent';
 
 
-// TODO Ask backend if user is authenticated?
-
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false
-    };
-  }
-
-  
-  isAuthenticated() {
-    console.log("Checking authentication...");
+// TODO: Check auhtentication
+function isAuthenticated() {
+  console.log("Checking authentication...");
     fetch("http://localhost:9000/isLoggedIn", {method: 'GET'})
       .then(res => res.json())
       .then(res => this.setState({ isLoggedIn: res}))    
-  }
+}
 
-  render() {
-    return (
-      /* 
-      SO: What I do here is breaking the rules of hooks since this is from within
-      a class component. This needs to be solved next!
-      */
-
+function App() {
+  return (
       <div className="App">
           { SignIn() }
           { SignUp() }
       </div>
-    );
-  }
+  );
 }
+
 
 export default App;
